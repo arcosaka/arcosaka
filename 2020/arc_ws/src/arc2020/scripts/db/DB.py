e6d51997e5860215f1e6b27f3be5a95482f88063
woowayer <woowayer@gmail.com>
2021-10-16 00:53:56 +0900

    db更新

---
+++ 2020/arc_ws/src/arc2020/scripts/db/DB.py
@@ -0,0 +1,130 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# license removed for brevity
+
+# pythonでROSのソフトウェアを記述するときにimportするモジュール
+import rospy
+
+import sqlite
+import copy
+# 自分で定義したmessageファイルから生成されたモジュール
+from arc2020.msg import sql
+from arc2020.msg import database
+from arc2020.msg import maind
+
+CYCLES = 1 #処理周波数
+# 定数などの定義ファイルimport
+
+
+# class DB 定義
+class DB(object):
+    """
+    DBクラス
+    """
+#--------------------
+# コンストラクタ
+    def __init__(self):
+        """
+        コンストラクタ
+        """
+        self.cyclecount = 0
+        # 受信作成
+        self.sub_client  = rospy.Subscriber('sql', sql, self.insertCallback, queue_size=1)
+        self.sub_main  = rospy.Subscriber('maind', maind, self.mainCallback, queue_size=1)
+        # 送信作成
+        self.pub_database = rospy.Publisher('database', database, queue_size=100)
+        # messageのインスタンスを作る
+        self.msg_sql = sql()
+        self.msg_database = database()
+        self.msg_main = maind()
+        # dbを作成する
+        db_name = 'yasai'
+        self.db = sqlite.sqlite(db_name) 
+        print("db create by DB")
+
+        ##self.db.selectTable("yasai")
+        self.db.selectyasai1()
+        # tableを作成する
+        # self.table_seed = 'seed'
+        # self.sqlite.createTable(self.table_seed)
+
+#--------------------
+# 受信コールバック
+    def insertCallback(self, msg):
+        """
+        brainの受信コールバック
+        """
+        print("table " + msg.table + ",name " + msg.name)
+
+        # msg.tableにmsg.nameを登録する       
+        self.db.insert(msg.table,msg.name)
+
+    def mainCallback(self, msg):
+        """
+        mainの受信コールバック
+        """
+        print(msg.database_inforeq)
+        print(msg.database_vegetable_num1)
+        print(msg.database_vegetable_num2)
+        print(msg.database_ridge_width)
+        print(msg.database_ridge_length)
+
+        if msg.database_inforeq:
+           aaa = self.db.selectyasai1()
+           self.msg_database.mainseedlist = copy.copy(aaa)
+
+        bbb = ','.join(msg.database_vegetable_num1)
+        print (bbb)
+        self.msg_database.subseedlist = self.db.selectyasai2(bbb)
+        ccc = ','.join(msg.database_vegetable_num2)
+        print (ccc)
+        haichi = self.db.selectyasaihaichi(bbb,ccc,msg.database_ridge_width,msg.database_ridge_length)
+        print (haichi)
+
+#--------------------
+    def main(self):
+        # メッセージを発行する
+        #self.msg_database.mainseedlist = []
+        #aaa = self.db.selectyasai1()
+        #self.msg_database.mainseedlist = self.db.selectyasai1()
+        #for rows in aaa: 
+        #  print rows[0]
+          #self.msg_database.mainseedlist += rows[0]
+        #self.msg_database.mainseedlist = copy.copy(aaa)
+#        self.msg_database.mainseedlist = 'aaa'
+#        self.msg_database.subseedlist = 'bbb'
+        self.msg_database.seed_type = '0'
+        self.msg_database.coordinates_x = 20
+        self.msg_database.coordinates_y = 30
+        self.pub_database.publish(self.msg_database)
+        print('mainが動いていますよ！！')
+        #print(self.msg_database)
+        #for rows in self.msg_database.mainseedlist: 
+        #  print rows[0]
+        for rows in self.msg_database.subseedlist: 
+          print rows[0]
+
+def DB_py():
+    # 初期化宣言 : このソフトウェアは"DB_py_node"という名前
+    rospy.init_node('DB_py_node', anonymous=True)
+    # インスタンスの作成 
+    _DB = DB()
+    # 処理周期の設定
+    r = rospy.Rate(CYCLES)
+
+    print("[DB] start")
+
+
+    # ctl +　Cで終了しない限りwhileループで処理し続ける
+    while not rospy.is_shutdown():
+        # メイン処理
+        _DB.main()
+        #
+        r.sleep()
+
+if __name__ == '__main__':
+    try:
+        DB_py()
+
+    except rospy.ROSInterruptException:
+        print("[DB] end")
---
+++ 2020/arc_ws/src/arc2020/scripts/db/sqlite.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# license removed for brevity
+
+import sqlite3
+
+class sqlite(object):
+    def __init__(self,name_db):
+        path = "/root/catkin_ws/src/arc2020/"
+        print("db create start")
+        # 'dbname'.dbを作成する
+        # すでに存在していれば、それにアスセスする。
+        self.db = path + name_db + '.db'
+        conn = sqlite3.connect(self.db)
+        print("db create done")
+        # いったん閉じる
+        conn.close()
+
+    def open(self):
+        # sqliteを操作するカーソルオブジェクトを作成
+        self.conn = sqlite3.connect(self.db)
+        self.cursor = self.conn.cursor()
+
+    def close(self):
+        # データベースへのコネクションを閉じる。(必須)
+        self.cursor.close()
+        self.conn.close()
+
+    def createTable(self,name_table):
+        # db open
+        self.open()
+
+        # 'name_table'というtableがなければ作成する
+        # 大文字部はSQL文。小文字でも問題ない。
+        command = 'CREATE TABLE IF NOT EXISTS '+ name_table + ' ( id INTEGER PRIMARY KEY,name VARCHAR(20),price INTEGER);'
+        self.cursor.execute( command )
+        self.conn.commit()
+        
+        # db close
+        self.close()
+
+    def insert(self, name_table, name_seed):
+        # create table
+        self.createTable(name_table)
+        # db open
+        self.open()
+        # 'name_table'の"name"に"name_seed"を入れる
+        command = 'INSERT INTO '+ name_table +'(name)' + ' values("'+name_seed+'")'
+        self.cursor.execute(command)
+        #登録
+        self.conn.commit()
+    
+        # db close
+        self.close()
+
+    def selectTable(self,name_table):
+        # db open
+        self.open()
+
+        # terminalで実行したSQL文と同じようにexecute()に書く
+        command = 'SELECT * FROM ' + name_table 
+        self.cursor.execute( command )
+
+        # 中身を全て取得するfetchall()を使って、printする。
+        print(self.cursor.fetchall())
+
+        # db close
+        self.close()
+
+    def selectyasai1(self):
+        # db open
+        self.open()
+
+        #print 'printしますよ！！'
+        # terminalで実行したSQL文と同じようにexecute()に書く
+        command = 'SELECT 野菜1 FROM yasai group by 野菜1 having count(野菜1)>1' 
+        self.cursor.execute( command )
+
+        # 中身を全て取得するfetchall()を使って、printする。
+        moji = self.cursor.fetchall()
+
+        #for rows in moji: 
+        #  print rows[0]
+
+        # db close
+        # self.close()
+        return moji
+
+    def selectyasai2(self,vegetable_num1):
+        self.open()
+
+        #print 'printしますよ！！'
+        # terminalで実行したSQL文と同じようにexecute()に書く
+        command = 'SELECT 野菜2 FROM yasai WHERE 野菜1 = \'' + vegetable_num1 + '\'group by 野菜2'
+        #command = 'SELECT 野菜2 FROM yasai WHERE 野菜1 = \'小カブ\' group by 野菜2'
+        self.cursor.execute( command )
+
+        # 中身を全て取得するfetchall()を使って、printする。
+        moji = self.cursor.fetchall()
+
+        return moji
+
+    def selectyasaihaichi(self,vegetable_num1,vegetable_num2,ridge_width,ridge_length):
+        self.open()
+
+        print(ridge_width)
+        print(ridge_length)
+
+        command = 'SELECT 配置 FROM yasai WHERE 野菜1 = \'' + vegetable_num1 + '\' AND 野菜2 = \'' + vegetable_num2 + '\' AND 幅 = ' + ridge_width + ' AND 長さ = 10 '
+        #command = 'SELECT 配置 FROM yasai WHERE 野菜1 = \'' + vegetable_num1 + '\' AND 野菜2 = \'' + vegetable_num2 + '\' AND 幅 = 10 AND 長さ = 60 '
+        #command = 'SELECT 配置 from yasai where 野菜1 = \'小カブ\' AND 野菜2 = \'春菊\' AND 幅 = 10 AND 長さ = 60'
+        self.cursor.execute( command )
+
+        # 中身を全て取得するfetchall()を使って、printする。
+        moji = self.cursor.fetchall()
+
+        return moji
+
